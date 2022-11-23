4d4ca7aee689c06b7792923f2844f341e3b872e1
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 <galluccimia@gmail.com>
2022-11-22 22:16:37 -0500

    flask api queries

--- climateapp.py
+++ climateapp.py
@@ -4,6 +4,7 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+import datetime as dt
 
 from flask import Flask, jsonify
 
